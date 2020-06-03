1c62e69d97d5d755c231948a0bcb7e41f89785c8
aldipee <aldipeee@gmail.com>
2020-06-03 15:54:25 +0700

    add capitalize helper

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,13 @@ export const formatDate = (isoDate: string): string => {
   const d = new Date(isoDate)
   return d.toLocaleDateString('id-ID')
 }
+export const capitalize = (str: string) => {
+  if (str.length === 0) return str
+
+  const chunks = str.split(' ')
+  const capitalizeChunks = chunks.map(str => {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase()
+  })
+
+  return capitalizeChunks.join(' ')
+}
